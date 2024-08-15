6ca69eabf5b082a3a97330fc93a7b9990bd428f4
wangjiabao <840028401@qq.com>
2024-08-15 15:30:22 +0800

    ok

--- internal/logic/binancetradehistory/binancetraderhistory.go
+++ internal/logic/binancetradehistory/binancetraderhistory.go
@@ -1644,7 +1644,7 @@ func (s *sBinanceTraderHistory) PullAndOrderNewGuiTu(ctx context.Context) {
 	binancePositionMap = make(map[string]*entity.TraderPosition, 0)
 
 	// 执行
-	//num1 := 0
+	num1 := 0
 	for {
 		time.Sleep(28 * time.Millisecond)
 		start := time.Now()
@@ -1690,14 +1690,14 @@ func (s *sBinanceTraderHistory) PullAndOrderNewGuiTu(ctx context.Context) {
 			// 龟兔的数据
 			reqResData, retry, err = s.requestBinancePositionHistoryNew(traderNum, cookie, token)
 			//reqResData, retry, err = s.requestProxyBinancePositionHistoryNew("http://43.130.227.135:888/", traderNum, cookie, token)
-			//num1++
-			//if 0 == num1%200 {
-			//	fmt.Println(time.Now())
-			//	if 1 < len(reqResData) {
-			//		fmt.Println(reqResData[1])
-			//	}
-			//	fmt.Println(baseMoneyGuiTu.Val(), baseMoneyUser.Val())
-			//}
+			num1++
+			if 0 == num1%20000 {
+				fmt.Println(time.Now())
+				if 1 < len(reqResData) {
+					fmt.Println(reqResData[1])
+				}
+				fmt.Println(baseMoneyGuiTu.Val(), baseMoneyUser.Val(), baseMoneyUserTwo.Val(), baseMoneyUserThree.Val())
+			}
 
 			// 需要重试
 			if retry {
